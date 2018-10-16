e3eeabf4f7596d3116db819381f09fdd4a361342
NickkBright <gressssssssky@gmail.com>
2018-10-16 13:17:11 +0300

    added template configuration of drill

--- zookeeper/bin/Entrypoint-zk.sh
+++ zookeeper/bin/Entrypoint-zk.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-set -e
 
-	myid=$1
-	server1=$2
-	server2=$3
-	server3=$4
-	echo $myid > /zookeeper/myid
-	for i in {1..3}
-	do
-		echo "server.${i}=${server${i}}:2888:3888" >> ${ZOOKEEPER_CONF_DIR}/zoo.cfg
-	done
-
-	exec su-exec zookeeper ${ZOOKEEPER_HOME}/bin/zkServer.sh start-foreground
-
-exec "$@" 

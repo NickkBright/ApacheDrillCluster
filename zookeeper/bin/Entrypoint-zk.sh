95aa6360c6da6f8b0865eef8a2893461b5687b75
NickkBright <gressssssssky@gmail.com>
2018-10-18 14:32:34 +0300

    added zookeeper script

--- zookeeper/bin/Entrypoint-zk.sh
+++ zookeeper/bin/Entrypoint-zk.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+set -e
+
+myid=$1
+
+
+IFS=',' read -r -a ip_array <<< "$2"
+
+for index in ${ip_array[@]}; do
+    echo server.${index}=${ip_array[index]}:2888:3888 >> ${ZOOKEEPER_HOME}/conf/zoo.cfg
+done
+
+echo $myid > /zookeeper/myid

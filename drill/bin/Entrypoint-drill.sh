27226c9a633b865b2ad557f6eca5564e092c92d7
NickkBright <gressssssssky@gmail.com>
2018-10-19 14:56:21 +0300

    fixed script and cleared conf file

--- drill/bin/Entrypoint-drill.sh
+++ drill/bin/Entrypoint-drill.sh
@@ -3,4 +3,4 @@ set -e
 cluster_id=$1
 quorum=$2
 
-printf "drill.exec: {\n cluster-id: \"${cluster_id}\",\n zk: { \n connect:\"${quorum}\", \n root: \"drill\" \n } "
+printf "drill.exec: {\n cluster-id: \"${cluster_id}\",\n zk: { \n connect:\"${quorum}\", \n root: \"drill\" \n } " >> ${DRILL_HOME}/conf/drill-override.conf
